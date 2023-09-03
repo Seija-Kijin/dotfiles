59e9234bdc3653662ea401ca7926bf37958076fd
Seija-Kijin <Gasparramirez108@gmail.com>
2023-09-03 22:30:06 +0800

    Post install script skeleton

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
+printf '\033c'
+echo "Mankind is dead"
+sleep 0.8
+echo "Hell is full"
+sleep 0.8
+echo "Blood is fuel"
+sleep 0.8
+echo "Kawakijin's Dotfile Deployer loaded..."
+echo "Begining post install, please wait warmly..."
+
 
 
 # POST INSTALL
+# Install yay
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 
+# Promt to install packages 
+read -r -p "Would you like to install packages in your .pkglist? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then yay -Syu --needed - < pkglist.txt
+else echo "Skipping Packages"
+fi
 
-pacman -S --needed - < pkglist.txt
