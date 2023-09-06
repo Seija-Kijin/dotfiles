702ce839fdd944d7ba8aa863b110591e1563524b
Seija-Kijin <Gasparramirez108@gmail.com>
2023-09-06 22:02:27 +0800

    Added prompt for neofetch image

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sleep 0.8
 echo "Kawakijin's Dotfile Deployer loaded..."
 echo "Begining dotfile deployment, please wait warmly..."
 
-# Put the git clone thing here!!!
+user=$(whoami)
 
 # Promt to install packages 
 read -r -p "Would you like to install packages in the .pkglist? [y/N] " response
@@ -18,24 +18,28 @@ then yay -Syu --needed - < pkglist.txt
 else echo "Skipping Packages"
 fi
 
-echo "Disabling Akonadi"
-akonadictl stop && 
+#echo "Disabling Akonadi"
+#akonadictl stop
 
 echo "disabling Baloo"
 balooctl disable
 
 echo "Installing and applying polybar customisation"
 yay -S polybar
-mv -i .config/polybar ~/.config
+mv -i .config/polybar ~/.config/polybar
 
 echo "Applying Neofetch config"
+#Double check that this works
+mv -i .config/neofetch ~/.config/neofetch
+read -r -p "Do you have an image you would like to use in neofetch? (Nyarch is included in .config/neofetch/Nyarch.png) [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then printf "%s"  "Enter the path to the image (Example: ~/Desktop/Picture.png): " && read image && cp $image ~/.config/neofetch" && sed -i -e 's/THEIMAGEGOESHERE/$image/g' ~/.config/neofetch
+else echo "Skipping Neofetch Image"
+fi
 
-#echo -n "Enter your username: " 
-#read username
+                                                                                                                                    
 
 
 #TODO research printf and see if that's better than using echo for user input
-#TODO Add a neofetch file to /.config/neofetch             then add it to this script
-#TODO find a way to make the script prompt for neofetch image, if yes then use image provided by user, if no skip, if nyarch then nyarch if else then skip
 
 
